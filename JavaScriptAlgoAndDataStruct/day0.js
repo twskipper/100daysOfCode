897d652d9c352acda58cf2ba509a788b435ed482
skipper <skipper@aidigger.com>
2022-01-18 21:01:03 +0800

    day0

---
+++ JavaScriptAlgoAndDataStruct/day0.js
@@ -0,0 +1,34 @@
+// javascript comment
+// write code while learn javascript from freecodecamp.org
+
+// this is in-line comment
+/* 
+this is a multi-line comment
+*/
+
+let a = 10
+var b = "i am string"
+a = b
+var c,d,e
+console.log(a,c,d,e)
+d = b
+c = 10
+let g = 0;
+console.log(d,c,g)
+var studlyCapVar
+var properCamelCase
+var titleCaseOver
+
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+var camper = "James"
+var camper = "David"
+
+let person = "James"
+
+const FAV_PET = "Cats"
